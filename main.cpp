6f19e1b5b6a602c62ec749ba508946a99c1351db
saikreddy <37062792+saikreddy@users.noreply.github.com>
2018-03-09 19:29:11 +0530

    sai created a file

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <cmath>
+#include <vector>
+#include <list>
+
+using namespace  std;
+
+class Vertex{
+	float x_cor;
+	float y_cor;
+	float z_cor;	
+};	
+
+class Edge{
+	Vertex p1, p2;
+}
+
+class makethreeD{
+	top_view;
+	front_view;
+	side_view;
+public :
+
+		int create3D( top_view, front_view, side_view){
+			return 0;
+	}
+
+
+}
+class twoDto3D
+{
+public :
+		
+		/**
+		 creating the front view from an image
+		 */
+		void createFrontView(std::list<Points> plane){
+
+		}
+		
+		/**
+		 creating the side view from an image
+		 */		
+		void createSideView(std::list<Points> plane){
+
+		}
+
+		/**
+		 creating the top view from an image
+		 */
+		void createTopView(std::list<Points> plane ){
+
+		}
+
+}
+
+int main (void) {
+	return 0;
+}
